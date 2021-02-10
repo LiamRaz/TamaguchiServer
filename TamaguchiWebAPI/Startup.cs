49782a37c19a870e5d9f058e03d34a2c64c9c648
ahiya <ahiya@LAPTOP-I3HQS0D2>
2021-02-10 13:50:29 +0200

    i change

--- TamaguchiWebAPI/Startup.cs
+++ TamaguchiWebAPI/Startup.cs
@@ -15,7 +15,7 @@ namespace TamaguchiWebAPI
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container!
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 
         public IConfiguration Configuration { get; set; }
